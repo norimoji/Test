2a6a445b4e386e63034fd6fcbb6fcf5d97ddc93a
Phong <zimmerzing@gmail.com>
2015-12-05 18:03:30 +0000

    Added world class.

---
+++ app/src/main/java/com/example/phong/test/world.java
@@ -0,0 +1,12 @@
+package com.example.phong.test;
+
+/**
+ * Created by Phong on 05/12/2015.
+ */
+public class world {
+    private String comment ="";
+
+    public void doComment(String comemnt) {
+        this.comment = comment;
+    }
+}
---
+++ app/src/main/java/com/example/phong/test/world3.java
@@ -0,0 +1,12 @@
+package com.example.phong.test;
+
+/**
+ * Created by Phong on 05/12/2015.
+ */
+public class world3 {
+    private String comment ="";
+
+    public void doComment(String comemnt) {
+        this.comment = comment;
+    }
+}
